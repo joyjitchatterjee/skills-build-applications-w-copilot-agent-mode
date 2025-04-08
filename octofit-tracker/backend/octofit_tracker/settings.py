40862e0cb3d3ffede23cb86becde740dd97d0bed
Dr. Joyjit Chatterjee <joyjitece@gmail.com>
2025-04-08 21:50:02 +0000

    Push changes to settings.py and other files

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Add the BASE_URL for the Django REST API endpoints
+BASE_URL = 'https://opulent-potato-5g76xgv9xgvhv6rv-8000.app.github.dev/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
